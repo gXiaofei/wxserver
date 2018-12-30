c60ecc14130c2eca18ba5ee9b0c9e69c09c58cb4
gxf <310877593.com>
2018-12-30 21:42:33 +0800

    detail

--- routes/art.js
+++ routes/art.js
@@ -6,49 +6,82 @@ var async = require('async');
 var moment = require('moment');
 
 // 按分页查询
-router.get('/info', function(req, res) {
+router.get('/info', function (req, res) {
     const params = req.query;
     pageQuery(parseInt(params.pageNo), parseInt(params.pageSize), artModel, '', {}, {
         artCreateTime: 'desc'
-    }, function(error, result, next){
-        if(error){
+    }, function (error, result, next) {
+        if (error) {
             return console.log(error);
-        }else{
+        } else {
             // 请求的页数 不能大于总页数
-            if(result.pageCount === 0){
+            if (result.pageCount === 0) {
                 res.send(result);
                 return;
             }
-            if(result.pageNumber <= result.pageCount){
+            if (result.pageNumber <= result.pageCount) {
                 res.send(result);
-            }else{
+            } else {
                 res.send('请求的页数不对');
             }
         }
     });
 })
+
+// 搜索
+router.get('/search', function (req, res) {
+    const params = req.query;
+    pageQuery(parseInt(params.pageNo), parseInt(params.pageSize), artModel, '', {
+        $or : [
+            {title : {$regex : params.value}},
+            {content : {$regex : params.value}}
+        ]
+    }, {
+        artCreateTime: 'desc'
+    }, function (error, result, next) {
+        if (error) {
+            return console.log(error);
+        } else {
+            // 请求的页数 不能大于总页数
+            if (result.pageCount === 0) {
+                res.send(result);
+                return;
+            }
+            if (result.pageNumber <= result.pageCount) {
+                res.send(result);
+            } else {
+                res.send('请求的页数不对');
+            }
+        }
+    });
+
+
+})
+
 // 新增
-router.post('/write', function(req, res) {
+router.post('/write', function (req, res) {
     var data = req.body;
     data.artCreateTime = new Date();
     var _model = new artModel({
         ...data
     });
     // 保存新增的内容
-    _model.save(function(err, result){
-        if(err) return console.log(err);
+    _model.save(function (err, result) {
+        if (err) return console.log(err);
         res.send(result);
     })
 })
 // 获取详情
-router.get('/detail', function (req, res){
+router.get('/detail', function (req, res) {
     const id = req.query.id
-    if(id){
-        artModel.findOne({_id: id}, function (err, data) {
-            if(err) return console.log(err); 
+    if (id) {
+        artModel.findOne({
+            _id: id
+        }, function (err, data) {
+            if (err) return console.log(err);
             res.send(data);
         })
-    }else{
+    } else {
         res.send('id传入有误')
     }
 })
@@ -60,12 +93,12 @@ var pageQuery = function (page, pageSize, Model, populate, queryParams, sortPara
         pageNumber: page
     };
     async.parallel({
-        count: function (done) {  // 查询数量
+        count: function (done) { // 查询数量
             Model.count(queryParams).exec(function (err, count) {
                 done(err, count);
             });
         },
-        records: function (done) {   // 查询一页的记录
+        records: function (done) { // 查询一页的记录
             Model.find(queryParams).skip(start).limit(pageSize).populate(populate).sort(sortParams).exec(function (err, doc) {
                 done(err, doc);
             });
